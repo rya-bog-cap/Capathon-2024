c172c708a3623b014aa1782663a145042ba3c435
ZouhairEM <zouhairelmariami@gmail.com>
2024-03-29 11:07:55 +0100

    formatted all files with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,46 @@
-import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import './App.css';
-import BackgroundParallex from './components/BackgroundParallex';
-import Cards from './components/Cards';
-import './App.css';
-import CardItem from './components/CardItem';
-import BgGridVideo from './assets/videos/grid-loop.mp4';
-
-
-
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import BackgroundParallex from "./components/BackgroundParallex";
+import Cards from "./components/Cards";
+import "./App.css";
+import CardItem from "./components/CardItem";
+import BgGridVideo from "./assets/videos/grid-loop.mp4";
 
 function App() {
   return (
     <>
-    <Router>
-    <NavBar />
+      <Router>
+        <NavBar />
 
-    <video id="bg-video" >
-      <source src={BgGridVideo} type="video/mp4" />
-    </video>
+        <video id="bg-video">
+          <source src={BgGridVideo} type="video/mp4" />
+        </video>
 
-    {/* <BackgroundParallex /> */}
+        {/* <BackgroundParallex /> */}
 
-   
+        <section id="section-one">
+          <Cards id="row0" title="Row 0" />
+        </section>
 
-  <section id="section-one">
-    <Cards
-      id='row0'
-      title='Row 0'
-    />
-  </section>
+        <section id="section-two">
+          <Cards id="row1" title="Row 1" />
+        </section>
 
-  <section id="section-two">
-    <Cards 
-      id='row1'
-      title='Row 1'
-    />
-  </section>
+        <section id="section-three">
+          <Cards />
+        </section>
 
-  
-  <section id="section-three">
-    <Cards />
-  </section>
-  
-    <Cards />
-    
+        <Cards />
 
-    {/* This doesn't work and is unnecessary for this site because its one page, I think? Not sure
+        {/* This doesn't work and is unnecessary for this site because its one page, I think? Not sure
     <Routes>
       <Route path='/' exact element={<Home />} />
     </Routes> 
     */}
-    </Router>
+      </Router>
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function CardItem(props) {
   return (
     <>
-    <li id={props.id} className="cards-item">
-        <Link className='cards-item-link' to={props.path}> {/**  WONT BE NEEDING THE PATH?  */}
+      <li id={props.id} className="cards-item">
+        <Link className="cards-item-link" to={props.path}>
+          {" "}
+          {/**  WONT BE NEEDING THE PATH?  */}
           <figure className="cards-item-pic-wrap">
-            <img className="cards-item-img" 
-            src={props.src} 
-            alt={props.alt} 
-            />
+            <img className="cards-item-img" src={props.src} alt={props.alt} />
           </figure>
           <div className="cards-item-info">
             <h5 className="cards-item-text">{props.text}</h5>
           </div>
         </Link>
-    </li>
+      </li>
     </>
   );
 }
 
-export default CardItem
+export default CardItem;
