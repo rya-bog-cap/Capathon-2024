a1a9cd80d3ba9a1c79af2a71b2f94ceb184773c4
DiNoasaur <noavanb@gmail.com>
2024-05-11 00:37:40 +0200

    scroll ayy-yo-zouhair-u-missed-this

--- src/components/parallex.jsx
+++ src/components/parallex.jsx
@@ -1,10 +1,33 @@
+this.vidTimeDelay = true;
 window.addEventListener('scroll', function () {
-  let vid = document.getElementById('bg-video');
+  const vid = document.getElementById('bg-video'); 
   vid.pause();
 
-  this.setInterval(function () {
-    vid.currentTime = window.scrollY / 1000;
-  }, 100);
+  const scrollMultiplier = 0.2;
+
+  if(this.vidTimeDelay){
+    var currTime = 
+      (
+        //document.documentElement.scrollTop *
+        (window.scrollY * 
+        scrollMultiplier) /
+        240
+      )
+    ;
+    if(currTime < 0)
+      currTime = 0.01;
+    if(currTime > vid.duration)
+      currTime %= vid.duration;
+    if(isNaN(currTime) || !isFinite(currTime))
+      currTime = 0.0;
+    console.log(`ct:${currTime} | vt:${vid.currentTime}`);
+    
+    vid.currentTime = currTime;
+    this.vidTimeDelay = false;
+    this.setTimeout(() => { this.vidTimeDelay = true; }, 100);
+  }
+
+  //}, 200);
 
   // setTimeout(() => {
   //   document.querySelector("h3").classList.add("animated-title");
