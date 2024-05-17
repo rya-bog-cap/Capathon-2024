2a5fec66f5cec23ffb4c67d9af0780e4c7dac193
Ryan van den Bogaard <bogaardryan@hotmail.com>
2024-05-17 16:22:40 +0200

    zouhairs mix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Cards from './components/Cards';
 import './App.css';
+import { useState } from 'react';
 import BgGridVideo from './assets/videos/grid-loop.mp4';
 import tomGif from './assets/videos/tomOutro.gif';
 import CardHero from './components/CardHero';
@@ -16,10 +17,85 @@ import portraitZouhair from './assets/images/Zouhair.jpg';
 import portraitRyan from './assets/images/Ryan0.jpg';
 import portraitMartin from './assets/images/Martin.jpg';
 import portraitNoa from './assets/images/Noa.jpg';
+import { Button } from './components/Button';
 
 import pixelCap from './assets/images/pixelCap.png';
 
+const pastEvents = {
+  2023: {
+    images: [
+      {
+        original: './DSC00138.JPG',
+        thumbnail: './DSC00138.JPG',
+      },
+      {
+        original: './DSC00172.JPG',
+        thumbnail: './DSC00172.JPG',
+      },
+      {
+        original: './DSC01442.JPG',
+        thumbnail: './DSC01442.JPG',
+      },
+    ],
+    year: '2023',
+    text: "The 2023 content: I is not fat, i is fluffy spend all night ensuring people don't sleep sleep all day run at 3am and chill on the couch table yet hate dog, and reward the chosen human with a slow blink. Grass smells good haha you hold me hooman i scratch catty ipsum. Going to catch the red dot today going to catch the red dot today lick       meow at it i do a wiggle come here birdy step on your keyboard while you're gaming and then turn in a circle.",
+  },
+  2022: {
+    images: [
+      {
+        original: './DSC00138.JPG',
+        thumbnail: './DSC00138.JPG',
+      },
+      {
+        original: './DSC00172.JPG',
+        thumbnail: './DSC00172.JPG',
+      },
+      {
+        original: './DSC01442.JPG',
+        thumbnail: './DSC01442.JPG',
+      },
+    ],
+    year: '2022',
+    text: "The 2022 content: I is not fat, i is fluffy spend all night ensuring people don't sleep sleep all day run at 3am and chill on the couch table yet hate dog, and reward the chosen human with a slow blink. Grass smells good haha you hold me hooman i scratch catty ipsum. Going to catch the red dot today going to catch the red dot today lick       meow at it i do a wiggle come here birdy step on your keyboard while you're gaming and then turn in a circle.",
+  },
+  2021: {
+    images: [
+      {
+        original: './DSC00138.JPG',
+        thumbnail: './DSC00138.JPG',
+      },
+      {
+        original: './DSC00172.JPG',
+        thumbnail: './DSC00172.JPG',
+      },
+      {
+        original: './DSC01442.JPG',
+        thumbnail: './DSC01442.JPG',
+      },
+    ],
+    year: '2021',
+    text: "The 2021 content: I is not fat, i is fluffy spend all night ensuring people don't sleep sleep all day run at 3am and chill on the couch table yet hate dog, and reward the chosen human with a slow blink. Grass smells good haha you hold me hooman i scratch catty ipsum. Going to catch the red dot today going to catch the red dot today lick       meow at it i do a wiggle come here birdy step on your keyboard while you're gaming and then turn in a circle.",
+  },
+};
+
 function App() {
+  const [selectedYear, setSelectedYear] = useState(pastEvents['2023']);
+
+  function selectYear(num) {
+    switch (num) {
+      case 2023: {
+        setSelectedYear(pastEvents['2023']);
+        break;
+      }
+      case 2022: {
+        setSelectedYear(pastEvents['2022']);
+        break;
+      }
+      default: {
+        setSelectedYear(pastEvents['2021']);
+      }
+    }
+  }
   return (
     <>
       <Router>
@@ -33,15 +109,48 @@ function App() {
           <CardHero />
         </section>
 
-        <section id="section-two">
-          {/* <h1>Past Events</h1> */}
-          <CardCaroussell id="card-index0" cardDirectionCSS={'cards-item-left'} />
-          <CardText cardDirectionCSS={'cards-item-right'} />
-        </section>
-
-        <section>
-          <CardText cardDirectionCSS={'cards-item-left'} />
-          <CardCaroussell id="card-index0" cardDirectionCSS={'cards-item-right'} />
+        <section id="section-two" className="flex flex-col gap-6">
+          <h1 className="">Past Events</h1>
+          <div className="flex gap-6">
+            <div className="w-3/5">
+              <CardCaroussell
+                id="card-index0"
+                cardDirectionCSS={'cards-item-left'}
+                images={selectedYear.images}
+              />
+            </div>
+            <CardText
+              cardDirectionCSS={'cards-item-right'}
+              year={selectedYear.year}
+              text={selectedYear.text}
+            />
+          </div>
+          <div className="flex gap-2">
+            <Button
+              onClick={() => selectYear(2023)}
+              buttonStyle="btn-medium"
+              buttonSize="btn--medium"
+              isActive={selectedYear.year === '2023'}
+            >
+              2023
+            </Button>
+            <Button
+              onClick={() => selectYear(2022)}
+              buttonStyle="btn-medium"
+              buttonSize="btn--medium"
+              isActive={selectedYear.year === '2022'}
+            >
+              2022
+            </Button>
+            <Button
+              onClick={() => selectYear(2021)}
+              buttonStyle="btn-medium"
+              buttonSize="btn--medium"
+              isActive={selectedYear.year === '2021'}
+            >
+              2021
+            </Button>
+          </div>
         </section>
 
         <section id="section-three">
