07ddcd5536a40720991ae7de4a4c95725f8b7488
Ryan van den Bogaard <bogaardryan@hotmail.com>
2024-05-17 16:22:40 +0200

    zouhairs mix

--- src/components/CardText.jsx
+++ src/components/CardText.jsx
@@ -1,6 +1,6 @@
 import './cards.css';
 
-function CardText({ id, src, alt, text, description, cardDirectionCSS }) {
+function CardText({ id, src, alt, text, year, description, cardDirectionCSS }) {
   return (
     <>
       <li id={id} className={`cards-item ${cardDirectionCSS}`}>
@@ -10,16 +10,8 @@ function CardText({ id, src, alt, text, description, cardDirectionCSS }) {
           </figure>
 
           <div className="card-content-text">
-            <h2>Capathon 2022</h2>
-            <p>
-              I is not fat, i is fluffy spend all night ensuring people don't sleep sleep all day
-              run at 3am and chill on the couch table yet hate dog, and reward the chosen human with
-              a slow blink. Grass smells good haha you hold me hooman i scratch catty ipsum. Going
-              to catch the red dot today going to catch the red dot today lick master's hand at
-              first then bite because im moody for i see a bird i stare at it i meow at it i do a
-              wiggle come here birdy step on your keyboard while you're gaming and then turn in a
-              circle.
-            </p>
+            <h2>Capathon {year}</h2>
+            <p>{text}</p>
           </div>
         </div>
       </li>
