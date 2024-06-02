9112d2db7cf2c72c4ef4e42c9a09e7608779c0fe
Ryan van den Bogaard <bogaardryan@hotmail.com>
2024-06-02 23:50:39 +0200

    new title shit

--- src/components/CardHero.jsx
+++ src/components/CardHero.jsx
@@ -5,11 +5,27 @@ function CardHero({ id, src, alt, text, description, cardDirectionCSS }) {
     <>
       <li id={id} className={`cards-item cards-background-blue ${cardDirectionCSS}`}>
         <div className="cards-item-link">
+
+          <table className="capthon-details-table">
+            <tr>
+              <td><h2>Theme</h2></td>
+              <td><p>To be revealed</p></td>
+            </tr>
+            <tr>
+              <td><h2>Date</h2></td>
+              <td><p>25-26 October 2024</p></td>
+            </tr>
+            <tr>
+              <td><h2>Location</h2></td>
+              <td><p>Van der Valk hotel Blijdorp (Rotterdam)</p></td>
+            </tr>
+          </table>
+
           {/* <figure className="cards-item-pic-wrap">
             <img className="cards-item-img" src={src} alt={alt} />
           </figure> */}
-
-          <ul className="card-titles">
+          
+          {/* <ul className="card-titles">
             <li>
               <h2>Theme</h2>
             </li>
@@ -32,7 +48,7 @@ function CardHero({ id, src, alt, text, description, cardDirectionCSS }) {
             <li>
               <p>Van der Valk hotel Blijdorp (Rotterdam)</p>
             </li>
-          </ul>
+          </ul> */}
         </div>
       </li>
     </>
